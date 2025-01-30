0fff40e3b58119b96eea238499af2943e550f05b
DaniM0613 <wwh.rj.92@gmail.com>
2025-01-29 23:02:15 -0600

    Creating the update endpoint

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main(){
 		return c.Status(200).JSON(fiber.Map{"msg": "Hello Word"})
 	})
 
-	// To post a TODO
+	// Create a TODO
 	app.Post("/api/todos",  func(c *fiber.Ctx) error {
 		todo := &Todo{}  
 
@@ -41,6 +41,20 @@ func main(){
 	   todos = append(todos, *todo)
 
 	   return c.Status(201).JSON(todo)
+	})
+
+	// update a TODO
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+              todos[i].Completed = true
+			  return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(400).JSON(fiber.Map{"error": "Todo not found"})
 	})
 
     log.Fatal(app.Listen(":4000"))
