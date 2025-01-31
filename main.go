336128114ca8002f8fa03a37d7d0dcec969b3f43
DaniM0613 <wwh.rj.92@gmail.com>
2025-01-31 15:42:41 -0600

    Configurando mongodb, descargando mongo-driver y codificandolo

--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
+import (    
+	"fmt"
+	"log"
+	"os"
+	"context"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+
+)
+
+type Todo struct {
+	ID         int    `json: "id" bson: "_id"`
+	Completed  bool   `json: "completed"`
+	Body	   string `json: "body"`
+}
+
+var collection  *mongo.Collection
+
+func main(){
+	fmt.Println("Hello World")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MONGODB ATLAS")
+}
+
+
+
+
+
+
+
+
+/*package main
+
 import (
 	"fmt"
 	"log"
@@ -82,4 +135,4 @@ func main(){
 	})
 
     log.Fatal(app.Listen(":"+PORT))
-}
+}*/
